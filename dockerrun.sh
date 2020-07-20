5f138dd99225cbe79bd25a62e67adf121ce8711d
sanggggg <ksme6776@gmail.com>
2020-07-20 13:56:00 +0900

    docker configure

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,5 +1,5 @@
 sudo nvidia-docker run -itd \
-    -v /home/sanggggg/dataset/blade_detection/darknet/20191107_siwha/191107_133745_8633:/dataset \
+    -v /home/sanggggg/dataset/blade_detection/darknet/:/home/sanggggg/dataset/blade_detection/darknet \
     -v /home/sanggggg/repos/darknet:/app \
     -p 8080:80 \
     --name darknet-train \
--- generate_trainval.py
+++ generate_trainval.py
@@ -2,61 +2,40 @@ import glob
 import argparse
 import os
 import xml.etree.ElementTree as ET
+from sklearn.model_selection import train_test_split
+
+object_enum = {'blade':0, 'nose':1, 'pole':2}
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input_dirs", nargs="+", help="xml directory")
-parser.add_argument("-r", "--random", default=False, help="shuffle and get date", required=False)
+parser.add_argument("-o", "--output_dir", default="./data", help="directory for train.txt, test.txt")
+parser.add_argument("-r", "--ratio", default=0.2, type=float, help="test set ratio")
 
 args = parser.parse_args()
 
 input_dirs = args.input_dirs
+output_dir = args.output_dir
+ratio = args.ratio
+
+filenames = []
 
 for i in input_dirs:
     if not os.path.isdir(i):
         print(f"input dir not exists: '{i}'")
         exit(0)
+    filenames += list(map(lambda x: x.replace("txt", "jpg").replace("labels", "JPEGImages"), glob.glob(os.path.join(i, "*.txt"))))
+
+train_list, validation_list = train_test_split(filenames, test_size=ratio, random_state=11)
 
 
-for i in input_dirs:
+train_txt = os.path.join(output_dir, "train.txt")
+validation_txt = os.path.join(output_dir, "test.txt")
 
-filenames = glob.glob(os.path.join(input_dir, "*.xml"))
+with open(train_txt, "w") as f:
+    f.writelines("\n".join(train_list))
 
-object_enum = {'blade':0, 'nose':1, 'pole':2}
-docker_volume_prefix = "/home/sanggggg/dataset/blade_detection/darknet/20191107_siwha/191107_135753_8268/JPEGImages"
+with open(validation_txt, "w") as f:
+    f.writelines("\n".join(validation_list))
 
-process_count = 0
-
-with open(train_txt, "w") as train_txt:
-    for filename in filenames:
-        
-        tree = ET.parse(filename)
-        root = tree.getroot()
-        
-        jpgname = tree.find('filename').text
-        txtname = tree.find('filename').text.split(".")[0] + ".txt"
-
-        img_width = float(tree.find('.//width').text)
-        img_height = float(tree.find('.//height').text)
-
-        
-        with open(os.path.join(output_dir, txtname), "w") as f:
-            process_count += 1
-            f.truncate(0)
-
-            train_txt.writelines(os.path.join(docker_volume_prefix, jpgname) + "\n")
-            print(f"processing {process_count}/{len(filenames)} : {txtname}")
-            for bbox in root.findall('object'):
-                cid = object_enum[bbox.findtext('name')]
-                xmin = float(bbox.findtext('.//xmin'))
-                ymin = float(bbox.findtext('.//ymin'))
-                xmax = float(bbox.findtext('.//xmax'))
-                ymax = float(bbox.findtext('.//ymax'))
-
-                bbox_width_ratio = (xmax - xmin) / img_width
-                bbox_height_ratio = (ymax - ymin) / img_height
-
-                bbox_x_center_ratio = (xmax + xmin) / 2 / img_width
-                bbox_y_center_ratio = (ymax + ymin) / 2 / img_height
-
-                f.write("%d %f %f %f %f" % (cid, bbox_x_center_ratio, bbox_y_center_ratio, bbox_width_ratio, bbox_height_ratio))
+print("Train & Test set saved at %s." % "./data")
